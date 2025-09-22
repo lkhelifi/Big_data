a53bbe42cf453d3f677fbd7c774d3818f32fd33e
Lidia Kh <khelif_l@etna-alternance.net>
2025-09-22 14:56:53 +0200

    exercice 01

---
+++ producer.py
@@ -0,0 +1,15 @@
+from kafka import KafkaProducer
+import json
+
+# Connexion au broker Kafka (nom du service docker-compose)
+producer = KafkaProducer(
+    bootstrap_servers=['kafka:9092'],
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+)
+
+# Message JSON
+message = {"msg": "Hello Kafka"}
+producer.send('weather_stream', value=message)
+
+producer.flush()
+print("Message envoyé :", message)
